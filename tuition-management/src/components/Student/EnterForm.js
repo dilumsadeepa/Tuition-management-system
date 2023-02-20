252b9fa1f80c0db61ff3dc8dece071013e5d4312
Madhuka Priyashan <111411737+Madhuka164@users.noreply.github.com>
2023-02-20 15:57:14 +0530

    Update student enter form

--- tuition-management/src/components/Student/EnterForm.js
+++ tuition-management/src/components/Student/EnterForm.js
@@ -2,25 +2,48 @@ import React, { useState } from 'react';
 
 
 function EnterForm() {
-  const [name, setName] = useState('');
+  const [fullname, setFName] = useState('');
+  const [ininame, setIName] = useState('');
+  const [Address, setAddress] = useState('');
   const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
+  const [NIC, setNIC] = useState('');
+
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log({ name, email, message });
-    // do something with the form data, like submit it to a server
+    console.log({ fullname,ininame,Address, email,NIC});
+    // form data, like submit it to a server
   };
 
   return (
 
     <form onSubmit={handleSubmit}>
-      <label htmlFor="name">Name:</label>
-      <input type="text" id="name" name="name" value={name} onChange={(e) => setName(e.target.value)} /><br /><br />
-      <label htmlFor="email">Email:</label>
-      <input type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} /><br /><br />
-      <label htmlFor="message">Message:</label>
-      <textarea id="message" name="message" value={message} onChange={(e) => setMessage(e.target.value)}></textarea><br /><br />
+
+    <div class="mb-3 mt-3">
+      <label htmlFor="fullname" class="form-label">Full Name:</label>
+      <input type="text" id="fullname" name="fullname" class="form-control" value={fullname} onChange={(e) => setFName(e.target.value)} /><br /><br />
+    </div>
+
+    <div class="mb-3 mt-3">
+      <label htmlFor="ininame" class="form-label">Name with initials:</label>
+      <input type="text" id="ininame" name="ininame" class="form-control" value={ininame} onChange={(e) => setIName(e.target.value)} /><br /><br />
+    </div>
+
+    <div class="mb-3 mt-3">
+      <label htmlFor="Address" class="form-label">Address:</label>
+      <input type="text" id="Address" name="Address" class="form-control" value={Address} onChange={(e) => setAddress(e.target.value)} /><br /><br />
+    </div>
+
+    <div class="mb-3 mt-3">
+      <label htmlFor="email" class="form-label">Email:</label>
+      <input type="email" id="email" name="email" class="form-control" value={email} onChange={(e) => setEmail(e.target.value)} /><br /><br />
+    </div>
+
+    <div class="mb-3 mt-3">
+      <label htmlFor="NIC" class="form-label">NIC:</label>
+      <input type="nic" id="nic" name="nic" class="form-control" value={NIC} onChange={(e) => setNIC(e.target.value)} /><br /><br />
+    </div>
+
       <button type="submit">Submit</button>
     </form>
 
