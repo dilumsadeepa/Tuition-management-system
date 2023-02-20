90498da5ce73ab158daa3b670c795629d5d098d8
Madhuka Priyashan <111411737+Madhuka164@users.noreply.github.com>
2023-02-20 14:10:38 +0530

    student registered

--- tuition-management/src/App.js
+++ tuition-management/src/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Register from "./components/User/Register";
+import StudentReg from "./components/Student/Register";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
           <Route path="/" element={<Register />} />
-          
+          <Route path="Stureg" element={<StudentReg/>}/>
       </Routes>
    </BrowserRouter>
   );
