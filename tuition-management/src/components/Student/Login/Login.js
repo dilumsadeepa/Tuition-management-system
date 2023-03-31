196e1a892118b88d0ccc363716776f31fa404fa2
Madhuka Priyashan <madhuka164@gmail.com>
2023-03-31 09:39:25 +0530

    configured

--- tuition-management/src/components/Student/Login/Login.js
+++ tuition-management/src/components/Student/Login/Login.js
@@ -1,8 +1,25 @@
-import React, { useState } from 'react';
-import '../../App.css';
-import Header from '../../Header';
-import Footer from '../../Footer';
+import React, {useContext,useRef,useState} from 'react'
+import {link} from 'react-router-dom'
+import './login.css';
+import axios from 'axios';
+import {toast} from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
 
-function Login() {
+import logo from './gb.png';
+import { Context } from 'react';
 
-};
+const Login =()=>{
+    const navigate=useNavigate()
+            const emailRef=useRef();
+            const passRef=useRef();
+
+    const {dispatch,isFetching}=useContext(Context);
+
+const handleSubmit=async()=>{
+    dispatch({type :'LOGIN_START'});
+    const loginCred={
+        email:emailRef.current.value,
+        password:passRef.current.value,
+    }
+}
+}
