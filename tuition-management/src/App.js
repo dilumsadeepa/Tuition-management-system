8b6f190ff042bd0b4d8b0c2624eea327d478d5fe
Dilum Sadeepa <51749100+dilumsadeepa@users.noreply.github.com>
2023-03-31 21:50:01 +0530

    updated

--- tuition-management/src/App.js
+++ tuition-management/src/App.js
@@ -34,7 +34,7 @@ function App() {
 
 
           {/* student part */}
-          <Route path="StudentLogin" element={<Login/>}/>
+          {/* <Route path="StudentLogin" element={<Login/>}/> */}
 
           <Route path="Stureg" element={<StudentReg/>}/>
           <Route path="EnterForm" element={<EnterForm/>}/>
