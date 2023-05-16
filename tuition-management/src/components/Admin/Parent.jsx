db9cf995becd31bcb61170fecb46c705d32723e9
Dhananjaya Maveekumbura <80608433+dhananjaya889@users.noreply.github.com>
2023-05-16 20:56:09 +0530

    parents

--- tuition-management/src/components/Admin/Parent.jsx
+++ tuition-management/src/components/Admin/Parent.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from 'axios';
 import Apiurl from '../Apiurl';
@@ -10,6 +10,26 @@ import { Link } from 'react-router-dom';
 const Parent = () => {
 
     const { id } = useParams();
+    const [parent, setParent] = useState([]);
+   
+
+    const getPa = async (e) => {
+        try {
+            const response = await axios.get(`${Apiurl}/getpadata/${id}`);
+            setParent(response.data);
+            
+
+
+        } catch (error) {
+            console.log("error in getting data")
+        }
+    }
+
+    //axios call api
+    useEffect(() => {
+        getPa();
+    });
+
 
 
     return (
@@ -30,23 +50,59 @@ const Parent = () => {
                     <main class="py-6 bg-surface-secondary">
 
                         <div className="container py-5">
-
+                            
                             <div className="row g-6 mb-6">
-                                <Link to={`/singlestudent/${id}`} className="">
-                                <div class="col-xl-3 col-sm-6 col-12 debtn">
+                                <Link to={`/addparent/${id}`} className="">
+                                    <div class="col-xl-3 col-sm-6 col-12">
+                                        <div class="card shadow border-0">
+                                            <div class="card-body debtn">
+                                                <div class="row">
+                                                    <div class="col">
+                                                        <h3 style={{ color: "white" }}>Add Parent Data</h3>
+                                                    </div>
+
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </Link>
+                            </div>
+                           
+                            <div className="row mt-3 mb-3">
+                                <div className="col-sm-6 mx-auto">
                                     <div class="card shadow border-0">
                                         <div class="card-body">
                                             <div class="row">
                                                 <div class="col">
-                                                    <span class="h6 font-semibold text-muted text-sm d-block mb-2">Budget</span>
-                                                    <span class="h3 font-bold mb-0">$750.90</span>
+                                                    <table className="table">
+                                                        {parent.map((p) =>
+                                                            <tbody className="bg-dark" style={{color:"white"}}>
+                                                                <tr>
+                                                                    <th>Name</th>
+                                                                    <td>{p.username}</td>
+                                                                </tr>
+                                                                <tr>
+                                                                    <th>Ful Name</th>
+                                                                    <td>{p.pfullname}</td>
+                                                                </tr>
+                                                                <tr>
+                                                                    <th>Email</th>
+                                                                    <td>{p.email}</td>
+                                                                </tr>
+                                                                <tr>
+                                                                    <th>Phone Number</th>
+                                                                    <td>{p.tel}</td>
+                                                                </tr>
+                                                            </tbody>
+                                                        )}
+
+                                                    </table>
+                                                    
                                                 </div>
-                                                
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                                </Link>
                             </div>
 
 
