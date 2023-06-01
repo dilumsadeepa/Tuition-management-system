09cbad6059366ae3a0e8759a637758fe930291de
Dilum Sadeepa <51749100+dilumsadeepa@users.noreply.github.com>
2023-06-01 11:06:42 +0530

    Update index.js

--- v2/backend/index.js
+++ v2/backend/index.js
@@ -1,22 +1,7 @@
-// const express = require("express");
-// const app = express();
-// import cors from "cors";
-
-
-// app.use(cors());
-// app.use(express.json());
-
-// app.get("/", function (req, res) {
-//   res.set("Conten-Type", "text/html; charset = utf-8");
-//   res.send("<h1>Hello... I am working...</h1>");
-// });
-
-// app.listen(process.env.PORT || 6000);
-
-import express from "express";
+const express = require("express");
+const app = express();
 import cors from "cors";
 
-const app = express();
 
 app.use(cors());
 app.use(express.json());
@@ -26,4 +11,19 @@ app.get("/", function (req, res) {
   res.send("<h1>Hello... I am working...</h1>");
 });
 
-app.listen(6000, ()=> console.log('Server up and running...'));
+app.listen(process.env.PORT || 6000);
+
+// import express from "express";
+// import cors from "cors";
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+
+// app.get("/", function (req, res) {
+//   res.set("Conten-Type", "text/html; charset = utf-8");
+//   res.send("<h1>Hello... I am working...</h1>");
+// });
+
+// app.listen(6000, ()=> console.log('Server up and running...'));
