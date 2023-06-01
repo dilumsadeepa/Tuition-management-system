0273746384121d0aeaf7cc6356ce3f7d3cf2745c
Dilum Sadeepa <51749100+dilumsadeepa@users.noreply.github.com>
2023-06-01 10:23:45 +0530

    serever update

--- v2/backend/index.js
+++ v2/backend/index.js
@@ -1,7 +1,22 @@
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
+// import cors from "cors";
+
+
+// app.use(cors());
+// app.use(express.json());
+
+// app.get("/", function (req, res) {
+//   res.set("Conten-Type", "text/html; charset = utf-8");
+//   res.send("<h1>Hello... I am working...</h1>");
+// });
+
+// app.listen(process.env.PORT || 6000);
+
+import express from "express";
 import cors from "cors";
 
+const app = express();
 
 app.use(cors());
 app.use(express.json());
@@ -11,4 +26,4 @@ app.get("/", function (req, res) {
   res.send("<h1>Hello... I am working...</h1>");
 });
 
-app.listen(process.env.PORT || 6000);
+app.listen(6000, ()=> console.log('Server up and running...'));
