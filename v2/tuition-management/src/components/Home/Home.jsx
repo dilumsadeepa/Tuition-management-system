94a7e271ef0a4945e78d7594146618e4e79cbe46
Lasith Randima <74246027+LasithRandima@users.noreply.github.com>
2023-06-03 10:38:53 +0530

    updatae home

--- v2/tuition-management/src/components/Home/Home.jsx
+++ v2/tuition-management/src/components/Home/Home.jsx
@@ -5,16 +5,16 @@ import Footer from './Footer'
 function Home() {
   return (
     <div>
-        <div className="row" style={{ '--bs-body-bg': 'var(--bs-blue)', background: 'var(--bs-info)', marginRight: 0, marginLeft: 0 }}>
+        <div className="row" style={{ '--bs-body-bg': 'var(--bs-blue)', background: 'var(--bs-info)', marginRight: 0, marginLeft: 0, color:'#000' }}>
   <div className="col d-xxl-flex justify-content-xxl-center" style={{ '--bs-body-bg': 'var(--bs-blue)', background: 'var(--bs-info)' }}>
     <div className="row container" style={{ background: 'var(--bs-info)', '--bs-body-bg': 'var(--bs-blue)' }}>
       <div className="col-lg-6 col-xl-4 col-xxl-6 d-flex justify-content-lg-center align-items-lg-center justify-content-xl-center align-items-xl-center justify-content-xxl-start align-items-xxl-center">
-        <img src="../img/susipwin logo.jpg" width="85" height="53" style={{ marginTop: 8, marginBottom: 8, marginRight: 22 }} />
-        <img className="topquote" src="../img/nena gunasusadi.png" width="210" height="25" style={{ marginTop: 15 }} />
+        <img src="../img/susipwin logo.jpg" alt='logo' width="85" height="53" style={{ marginTop: 8, marginBottom: 8, marginRight: 22 }} />
+        <img className="topquote" src="../img/nena gunasusadi.png" alt='logo' width="210" height="25" style={{ marginTop: 15 }} />
       </div>
-      <div className="col d-lg-flex d-xxl-flex justify-content-lg-center align-items-lg-center justify-content-xxl-center align-items-xxl-center" style={{ marginTop: 18 }}>
+      <div className="col d-lg-flex d-xxl-flex justify-content-lg-center align-items-lg-center justify-content-xxl-center align-items-xxl-center" style={{ marginTop: 0 }}>
         <p className="d-xl-flex justify-content-xl-center align-items-xl-center" style={{ fontSize: 14 }}>
-          <i className="icon-map" style={{ marginTop: 26, fontSize: 25, marginRight: 8 }}></i>
+          <i className="fa-regular fa-map" style={{ fontSize: 25, marginRight: 8 }}></i>
           Susipwin Anuradhapura,Town Hall Pl,Anuradhapura
         </p>
       </div>
@@ -26,7 +26,7 @@ function Home() {
           &nbsp; susipwinedu@gmail.com
         </p>
         <p className="d-xl-flex justify-content-xl-center align-items-xl-center" style={{ fontSize: 16, marginBottom: 10 }}>
-          <i className="icon-phone" style={{ fontWeight: 'bold' }}></i>&nbsp; 070124567
+        <i class="fa-solid fa-phone"></i>&nbsp; 070124567
         </p>
       </div>
     </div>
@@ -35,11 +35,137 @@ function Home() {
 
 
         <Navbar />
+
         <main>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae in temporibus iusto laudantium molestiae. Ex, excepturi! Quae tempore optio ullam repudiandae, maiores nemo? Ad laborum perspiciatis maxime porro ab, consectetur cumque dolorum quod odit asperiores neque repudiandae at doloremque dignissimos quas distinctio aliquid rem reprehenderit in, optio laboriosam ipsum eligendi.</p>
-            <h1 className='my-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste neque non hic libero. Itaque cumque eius tempore veniam quae voluptatum numquam animi alias, totam velit nemo, beatae est iste corporis illum molestiae, accusantium eveniet quas eligendi facilis? Rerum quod nobis quaerat qui sint similique facilis, quos beatae in velit dolores sit rem eum. Tempore rerum necessitatibus animi nemo molestiae labore voluptatibus dignissimos possimus dolore distinctio architecto, voluptatum quia cupiditate dolorem omnis quisquam adipisci numquam nulla consequuntur magnam. Autem obcaecati asperiores enim reiciendis quod ab unde necessitatibus, a molestias vero nesciunt quo vitae. Eveniet, qui! Ut sequi sint tenetur in provident est quod possimus porro. Distinctio neque sit magnam debitis nostrum aut saepe tenetur minima repellat et amet delectus quo quam aspernatur sint facilis, quia commodi optio eos blanditiis sequi! Veniam maiores, recusandae veritatis voluptate ex ipsam. Ab maiores ipsa ad obcaecati nulla ipsum sequi molestiae cupiditate provident reprehenderit debitis nemo perferendis hic, doloribus ratione quibusdam repellendus veritatis illum assumenda mollitia rem. Quaerat sapiente, voluptate delectus excepturi corrupti officia temporibus saepe consequatur sed, alias magnam ut ratione a veritatis! Aliquid error totam quasi dolorem, provident eius reiciendis dolor veritatis nulla perspiciatis laborum dignissimos placeat unde neque voluptatem voluptates culpa eum eaque.</h1>
-            <h1 className='my-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste neque non hic libero. Itaque cumque eius tempore veniam quae voluptatum numquam animi alias, totam velit nemo, beatae est iste corporis illum molestiae, accusantium eveniet quas eligendi facilis? Rerum quod nobis quaerat qui sint similique facilis, quos beatae in velit dolores sit rem eum. Tempore rerum necessitatibus animi nemo molestiae labore voluptatibus dignissimos possimus dolore distinctio architecto, voluptatum quia cupiditate dolorem omnis quisquam adipisci numquam nulla consequuntur magnam. Autem obcaecati asperiores enim reiciendis quod ab unde necessitatibus, a molestias vero nesciunt quo vitae. Eveniet, qui! Ut sequi sint tenetur in provident est quod possimus porro. Distinctio neque sit magnam debitis nostrum aut saepe tenetur minima repellat et amet delectus quo quam aspernatur sint facilis, quia commodi optio eos blanditiis sequi! Veniam maiores, recusandae veritatis voluptate ex ipsam. Ab maiores ipsa ad obcaecati nulla ipsum sequi molestiae cupiditate provident reprehenderit debitis nemo perferendis hic, doloribus ratione quibusdam repellendus veritatis illum assumenda mollitia rem. Quaerat sapiente, voluptate delectus excepturi corrupti officia temporibus saepe consequatur sed, alias magnam ut ratione a veritatis! Aliquid error totam quasi dolorem, provident eius reiciendis dolor veritatis nulla perspiciatis laborum dignissimos placeat unde neque voluptatem voluptates culpa eum eaque.</h1>
+        <section className="clean-block clean-hero" style={{ color: 'rgba(162,194,241,0.85)', background: 'url("../img/scenery/7.jpg"), var(--bs-red)', borderColor: 'var(--bs-gray-600)' }}>
+        <div className="text">
+          <h2 style={{ marginBottom: '25px' }}>Susipwin Higher Education Institute&nbsp;</h2>
+          <p style={{ margin: '-2px', marginTop: '0px', marginRight: '0px', marginBottom: '15px', marginLeft: '0px' }}> ටියුෂන් කලාවේ පුරෝගාමියා</p>
+          <p style={{ marginBottom: '30px' }}> Unlock Your Potential at Susipwin, Anuradhapura's Premier Tuition Class</p>
+          <button className="btn btn-outline-light btn-lg" type="button">Learn More</button>
+        </div>
+      </section>
+      <section className="clean-block clean-info dark">
+        <div className="container">
+          <div className="block-heading">
+            <h2 className="text-info">Info</h2>
+            <h3 className="text-info">අපි ගැන තොරතුරු</h3>
+            <p>Susipwin Tuition Class in Anuradhapura: Grade 6-13 classes, experienced instructors, personalized learning, exam-focused, holistic approach, proven success.</p>
+          </div>
+          <div className="row align-items-center">
+            <div className="col-md-6">
+              <img className="img-fluid rounded" src="../img/scenery/2.jpg" alt="image" />
+            </div>
+            <div className="col-md-6">
+              <h3>Who We Are?</h3>
+              <div className="getting-started-info">
+                <p>Susipwin Tuition Class in Anuradhapura is a premier educational institute offering classes for students from Grade 6 to Grade 13. With a focus on comprehensive education, experienced instructors, and personalized learning programs, Susipwin provides the necessary guidance and support to help students succeed academically. The institute places a strong emphasis on exam preparation, equipping students with effective strategies and resources to excel in their examinations. With well-equipped facilities and a proven track record of success, Susipwin Tuition Class is dedicated to empowering students and helping them achieve their full potential.</p>
+              </div>
+              <button className="btn btn-outline-primary btn-lg" type="button">Read More</button>
+            </div>
+          </div>
+          <div className="row align-items-center">
+            <div className="col-md-6">
+              <h3>අපි කවුරුන්ද?</h3>
+              <div className="getting-started-info">
+                <p>පාසල් අධ්‍යාපනය දැවැන්ත ලෙස බිඳවැටී ඇති මෙවන් අවධියක උසස් පෙළ උසස් ලෙස සමත්ව විශ්ව විද්‍යාල වරම් අත් කර ගැනීමට නම්, ඔබට නිවැරදි හා නිරවුල් අධ්‍යාපනයක් ලැබිය යුතුම ය. විෂය කරුණු පමණක් නොව ජීවිතය දිනාගන්නා ආකාරය පිළිබඳව ද එසේ ජීවිතය දිනාගත් අයගෙන් උපදෙස් හා කමටහන් ද ලැබිය යුතුය.<br />මේ සියල්ලම ඔබට ලැබෙන දිවයිනේ ඇති ජනප්‍රියම හා සාර්ථකම අධ්‍යාපන ආයතනය වන්නේ අනුරාධපුර “සිප්වින්“ අධ්‍යාපන ආයතනය බවට අපේ සුවහසක් දූ දරුවන් හා දෙමාපියන් සාක්ෂි දරයි.</p>
+              </div>
+              <button className="btn btn-outline-primary btn-lg" type="button">Read More</button>
+            </div>
+            <div className="col-md-6">
+              <img className="img-fluid rounded" src="../img/scenery/15.jpg" alt="" />
+            </div>
+          </div>
+        </div>
+      </section>
         </main>
+
+
+        <section className="clean-block features">
+      <div className="container">
+        <div className="block-heading">
+          <h2 className="text-info">Our Specialists&nbsp;</h2>
+          <h5 className="text-info">අපගේ විශේෂාංග</h5>
+          <p>
+            We stand out with expert instructors, personalized learning, exam excellence, interactive teaching, holistic development, and a proven track record of success.
+          </p>
+        </div>
+        <div className="row justify-content-center">
+          <div className="col-md-5 feature-box">
+            <i className="icon-user-following icon"></i>
+            <h4>
+              <strong>Expert Instructors:</strong>
+            </h4>
+            <p>Highly qualified and experienced instructors who specialize in their respective subjects.</p>
+          </div>
+          <div className="col-md-5 feature-box">
+            <i className="icon-pencil icon"></i>
+            <h4>
+              <strong>Track Record of Success</strong>
+            </h4>
+            <p>Demonstrated history of students achieving remarkable academic results and securing admissions to prestigious institutions.</p>
+          </div>
+          <div className="col-md-5 feature-box">
+            <i className="icon-refresh icon"></i>
+            <h4>
+              <strong>Interactive Teaching Methods</strong>
+            </h4>
+            <p>Engaging and interactive teaching methods that promote active participation and enhance learning outcomes.</p>
+          </div>
+          <div className="col-md-5 feature-box">
+            <i className="icon-notebook icon"></i>
+            <h4>
+              <strong>Exam Excellence</strong>
+            </h4>
+            <p>Focus on exam preparation with proven strategies, practice tests, and resources to achieve outstanding results.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+
+    <section className="clean-block slider dark">
+      <div className="container">
+        <div className="block-heading">
+          <h2 className="text-info">Gallery</h2>
+          <p>
+            Our gallery showcases the vibrant learning environment and engaging activities at Susipwin Tuition Class. Explore the images capturing our students' active participation, collaborative learning, and moments of academic achievement.
+          </p>
+        </div>
+        <div id="carousel-1" className="carousel slide" data-bs-ride="carousel">
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img className="w-100 d-block" src="../img/scenery/9.jpg" alt="Slide Image" />
+            </div>
+            <div className="carousel-item">
+              <img className="w-100 d-block" src="../img/scenery/15.jpg" alt="Slide Image" />
+            </div>
+            <div className="carousel-item">
+              <img className="w-100 d-block" src="../img/scenery/7.jpg" alt="Slide Image" />
+            </div>
+          </div>
+          <div>
+            <a className="carousel-control-prev" href="#carousel-1" role="button" data-bs-slide="prev">
+              <span className="carousel-control-prev-icon"></span>
+              <span className="visually-hidden">Previous</span>
+            </a>
+            <a className="carousel-control-next" href="#carousel-1" role="button" data-bs-slide="next">
+              <span className="carousel-control-next-icon"></span>
+              <span className="visually-hidden">Next</span>
+            </a>
+          </div>
+          <ol className="carousel-indicators">
+            <li data-bs-target="#carousel-1" data-bs-slide-to="0" className="active"></li>
+            <li data-bs-target="#carousel-1" data-bs-slide-to="1"></li>
+            <li data-bs-target="#carousel-1" data-bs-slide-to="2"></li>
+          </ol>
+        </div>
+      </div>
+    </section>
+
+
+
         <Footer />
     </div>
   )
