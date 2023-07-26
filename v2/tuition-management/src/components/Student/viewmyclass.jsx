1b8235e48062e10447dc83fddd28acdec9f1b440
Madhuka Priyashan <111411737+MadhukaJS@users.noreply.github.com>
2023-07-26 11:50:08 +0530

    card fixed

--- v2/tuition-management/src/components/Student/viewmyclass.jsx
+++ v2/tuition-management/src/components/Student/viewmyclass.jsx
@@ -2,50 +2,82 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Apiurl from '../Apiurl';
 import { useCookies } from 'react-cookie';
+import Sidebar from './StudentSidebar';
+import Dashhead from './Dashhead';
 
 const ViewMyClass = () => {
   const [enrolledCourses, setEnrolledCourses] = useState([]);
-  const [cookies, setCookie] = useCookies(['user']); 
-  console.log(cookies.id);
+  const [cookies, setCookie] = useCookies(['user']);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    // Assuming you have an endpoint to fetch the enrolled courses for the logged-in student
     const fetchEnrolledCourses = async () => {
       try {
-        const response = await axios.get(`${Apiurl}/mystucourse/${cookies.id}`); // Replace with the actual API endpoint to fetch enrolled courses
+        const response = await axios.get(`${Apiurl}/mystucourse/${cookies.id}`);
         setEnrolledCourses(response.data);
-        console.log(response.data);
+        setIsLoading(false);
       } catch (error) {
         console.log('Error fetching enrolled courses:', error);
+        setError('Error fetching enrolled courses. Please try again later.');
+        setIsLoading(false);
       }
     };
 
     fetchEnrolledCourses();
-  }, []);
+  }, [cookies.id]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  // Function to chunk the array into rows of 4 cards each
+  const chunkArray = (arr, size) => {
+    const chunkedArr = [];
+    for (let i = 0; i < arr.length; i += size) {
+      chunkedArr.push(arr.slice(i, i + size));
+    }
+    return chunkedArr;
+  };
+
+  // Chunk the enrolledCourses array into rows of 4 cards each
+  const coursesRows = chunkArray(enrolledCourses, 4);
 
   return (
-    <div className="container">
-        
-      <div className="row mt-3">
-        {enrolledCourses.map((course) => (
-          <div className="col-md-4 mb-4" key={course.id}>
-            <div className="card">
-              <img src={course.coursebanner} className="card-img-top" alt="Course Banner" style={{ height: '200px' }} />
-              <div className="card-body">
-                <h5 className="card-title">{course.coursename}</h5>
-                <p className="card-text">Rs. {course.courseprice}</p>
-                {/* Add more course details here as needed */}
-              </div>
+    <div className="d-flex flex-column flex-lg-row h-lg-full bg-surface-secondary">
+      <Sidebar />
+      <div className="h-screen flex-grow-1 overflow-y-lg-auto">
+        <Dashhead />
+        <div className="container">
+          {coursesRows.map((row, rowIndex) => (
+            <div className="row mt-3" key={rowIndex}>
+              {row.map((course) => (
+                <div className="col-md-3 mb-4" key={course.id}>
+                  <div className="card">
+                    <img
+                      src={course.coursebanner}
+                      className="card-img-top"
+                      alt="Course Banner"
+                      style={{ height: '200px' }}
+                    />
+                    <div className="card-body">
+                      <h5 className="card-title">{course.coursename}</h5>
+                      <p className="card-text">Rs. {course.courseprice}</p>
+                      {/* Add more course details here as needed */}
+                    </div>
+                  </div>
+                </div>
+              ))}
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
 };
 
 export default ViewMyClass;
-
-
-
-
