1b3dd67f9d264adb1814c17a1bb54decba1bfe06
Madhuka Priyashan <111411737+MadhukaJS@users.noreply.github.com>
2023-07-26 12:25:35 +0530

    icon update view my class

--- v2/tuition-management/src/components/Student/viewmyclass.jsx
+++ v2/tuition-management/src/components/Student/viewmyclass.jsx
@@ -6,85 +6,107 @@ import Sidebar from './StudentSidebar';
 import Dashhead from './Dashhead';
 
 const ViewMyClass = () => {
-  const [enrolledCourses, setEnrolledCourses] = useState([]);
-  const [cookies, setCookie] = useCookies(['user']);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
+    const [enrolledCourses, setEnrolledCourses] = useState([]);
+    const [cookies, setCookie] = useCookies(['user']);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const fetchEnrolledCourses = async () => {
-      try {
-        const response = await axios.get(`${Apiurl}/mystucourse/${cookies.id}`);
-        setEnrolledCourses(response.data);
-        setIsLoading(false);
-        console.log(response.data);
-      } catch (error) {
-        console.log('Error fetching enrolled courses:', error);
-        setError('Error fetching enrolled courses. Please try again later.');
-        setIsLoading(false);
-      }
+    useEffect(() => {
+        const fetchEnrolledCourses = async () => {
+            try {
+                const response = await axios.get(`${Apiurl}/mystucourse/${cookies.id}`);
+                setEnrolledCourses(response.data);
+                setIsLoading(false);
+                console.log(response.data);
+            } catch (error) {
+                console.log('Error fetching enrolled courses:', error);
+                setError('Error fetching enrolled courses. Please try again later.');
+                setIsLoading(false);
+            }
+        };
+
+        fetchEnrolledCourses();
+    }, [cookies.id]);
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>{error}</div>;
+    }
+
+    // Function to chunk the array into rows of 4 cards each
+    const chunkArray = (arr, size) => {
+        const chunkedArr = [];
+        for (let i = 0; i < arr.length; i += size) {
+            chunkedArr.push(arr.slice(i, i + size));
+        }
+        return chunkedArr;
     };
 
-    fetchEnrolledCourses();
-  }, [cookies.id]);
+    // Chunk the enrolledCourses array into rows of 4 cards each
+    const coursesRows = chunkArray(enrolledCourses, 4);
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
+    return (
+        <div className="d-flex flex-column flex-lg-row h-lg-full bg-surface-secondary">
+            <Sidebar />
+            <div className="h-screen flex-grow-1 overflow-y-lg-auto">
+                <Dashhead />
+                <div className="container">
+                    {coursesRows.map((row, rowIndex) => (
+                        <div className="row mt-3" key={rowIndex}>
+                            {row.map((course) => (
+                                <div className="col-md-3 mb-4" key={course.id}>
+                                    <div className="card">
+                                        <img
+                                            src={course.course.coursebanner}
+                                            className="card-img-top"
+                                            alt="Course Banner"
+                                            style={{ height: '200px' }}
+                                        />
+                                        <div className="card-body">
+                                            <h5 className="card-title">{course.course.coursename}</h5>
+                                            <p className="card-text">Rs. {course.course.courseprice}</p>
 
-  if (error) {
-    return <div>{error}</div>;
-  }
 
-  // Function to chunk the array into rows of 4 cards each
-  const chunkArray = (arr, size) => {
-    const chunkedArr = [];
-    for (let i = 0; i < arr.length; i += size) {
-      chunkedArr.push(arr.slice(i, i + size));
-    }
-    return chunkedArr;
-  };
+                                            {course.aprovel == 1 && (
+                                                <i
+                                                    className="fas fa-check-circle"
+                                                    style={{
+                                                        fontSize: '2rem',
+                                                        border: '2px solid #4caf50',
+                                                        borderRadius: '50%',
+                                                        padding: '0.2rem'
+                                                    }}
+                                                ></i>
 
-  // Chunk the enrolledCourses array into rows of 4 cards each
-  const coursesRows = chunkArray(enrolledCourses, 4);
+                                            )}
 
-  return (
-    <div className="d-flex flex-column flex-lg-row h-lg-full bg-surface-secondary">
-      <Sidebar />
-      <div className="h-screen flex-grow-1 overflow-y-lg-auto">
-        <Dashhead />
-        <div className="container">
-          {coursesRows.map((row, rowIndex) => (
-            <div className="row mt-3" key={rowIndex}>
-              {row.map((course) => (
-                <div className="col-md-3 mb-4" key={course.id}>
-                  <div className="card">
-                    <img
-                      src={course.course.coursebanner}
-                      className="card-img-top"
-                      alt="Course Banner"
-                      style={{ height: '200px' }}
-                    />
-                    <div className="card-body">
-                      <h5 className="card-title">{course.course.coursename}</h5>
-                      <p className="card-text">Rs. {course.course.courseprice}</p>
-                      {course.aprovel == 1 && (
-                        <p>Approved</p>
-                      )}
-                      {course.aprovel == 0 && (
-                        <p>Pending</p>
-                      )}
-                      {/* Add more course details here as needed */}
-                    </div>
-                  </div>
+                                            {course.aprovel == 0 && (
+                                                //<p>Pending</p>
+                                                <i
+                                                    className="fas fa-hourglass-half"
+                                                    style={{
+                                                        fontSize: '2rem',
+                                                        border: '2px solid #f1c40f', // Yellow border color for "Pending"
+                                                        borderRadius: '50%', // To make the border circular for a "pending" appearance
+                                                        padding: '0.2rem' // Adjust padding as needed
+                                                    }}
+                                                ></i>
+
+                                            )}
+                                            {/* Add more course details here as needed */}
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    ))}
                 </div>
-              ))}
             </div>
-          ))}
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default ViewMyClass;
