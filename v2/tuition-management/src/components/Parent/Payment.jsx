f07ef2205206e083fe1d650cdf59a4db1345fc46
Dhananjaya Maveekumbura <80608433+dhananjaya889@users.noreply.github.com>
2023-07-27 06:32:31 +0530

    kk

--- v2/tuition-management/src/components/Parent/Payment.jsx
+++ v2/tuition-management/src/components/Parent/Payment.jsx
@@ -87,9 +87,8 @@ return (
                                                     <thead>
                                                         <tr>
                                                             <th>ID</th>
-                                                            <th>CourseName</th>
-                                                            <th>Course Subject</th>
-                                                            <th>Student name</th>
+                                                            <th>Course Name</th>
+                                                            <th>Student Name</th>
                                                             <th>Payment On Month</th>
                                                             <th>Payment Date</th>
                                                         </tr>
@@ -108,7 +107,6 @@ return (
                                                             <tr key={py.id}>
                                                                 <td>{py.id}</td>
                                                                 <td>{py.coursename}</td>
-                                                                <td>{py.coursesubject}</td>
                                                                 <td>{py.username}</td>
                                                                 <td>{monthName}</td>
                                                                 <td>{py.createdAt}</td>
