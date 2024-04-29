fc34c58a3254536ed708627c0ccf3f94bb6c86d5
Dilum Sadeepa <51749100+dilumsadeepa@users.noreply.github.com>
2024-04-29 17:12:52 +0530

    Update Database.js

--- v2/backend/config/Database.js
+++ v2/backend/config/Database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const db = new Sequelize('database-1', 'admin', '<Dilum>1234', {
+const db = new Sequelize('tms', 'admin', '<Dilum>1234', {
   host: 'database-1.c7okm4qkiexq.us-east-1.rds.amazonaws.com',
   dialect: 'mysql'
 });
